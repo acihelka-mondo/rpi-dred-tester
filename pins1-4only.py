7fa6a2a6aa4bce6348050c234dcee990572c7e6b
Adam Cihelka <adam.cihelka@mondo.com.au>
2025-03-14 15:40:35 +1100

    commented out things to go full manual mode with pinctrl

--- pins1-4only.py
+++ pins1-4only.py
@@ -12,7 +12,7 @@ pin6 = 26
 GPIO.setmode(GPIO.BCM)
 
 # set pins1-4 as input, pull-down
-os.system("pinctrl 0,5,6,13 pd ip")
+#os.system("pinctrl 0,5,6,13 pd ip")
 # GPIO.setup(pin1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 # GPIO.setup(pin2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 # GPIO.setup(pin3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -36,6 +36,6 @@ if(GPIO.input(pin4) == 1):
     print("drm4")
 
 os.system('pinctrl | grep -e "GPIO0 " -e "GPIO5 " -e "GPIO6 " -e "GPIO13 " -e "GPIO19 " -e "GPIO26 "')
-GPIO.cleanup()
-os.system('pinctrl | grep -e "GPIO0 " -e "GPIO5 " -e "GPIO6 " -e "GPIO13 " -e "GPIO19 " -e "GPIO26 "')
+# GPIO.cleanup()
+# os.system('pinctrl | grep -e "GPIO0 " -e "GPIO5 " -e "GPIO6 " -e "GPIO13 " -e "GPIO19 " -e "GPIO26 "')
 
