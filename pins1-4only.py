8ee96e1e559031f6633223f2567fbe6d8641cb39
Adam Cihelka <adam.cihelka@mondo.com.au>
2025-03-14 15:00:38 +1100

    added back GPIO to grep removed :

--- pins1-4only.py
+++ pins1-4only.py
@@ -34,7 +34,7 @@ if(GPIO.input(pin3) == 1):
 if(GPIO.input(pin4) == 1):
     print("drm4")
 
-os.system('pinctrl | grep -e "0:" -e "5:" -e "6:" -e "13:" -e "19:" -e "26:"')
+os.system('pinctrl | grep -e "GPIO0" -e "GPIO5" -e "GPIO6" -e "GPIO13" -e "GPIO19" -e "GPIO26"')
 GPIO.cleanup()
-os.system('pinctrl | grep -e "0:" -e "5:" -e "6:" -e "13:" -e "19:" -e "26:"')
+os.system('pinctrl | grep -e "GPIO0" -e "GPIO5" -e "GPIO6" -e "GPIO13" -e "GPIO19" -e "GPIO26"')
 
